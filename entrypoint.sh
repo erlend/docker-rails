2bde379339b04ee4ae78573f5b0d13d992565f9e
Erlend Finvåg <erlend.finvag@gmail.com>
2018-06-30 16:29:26 +0200

    Run `bin/setup` instead of bundler if it exists

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,19 @@
 if [ $# -eq 0 ]; then
   set -- rails server -b 0.0.0.0
 fi
- 
-if [ "$1" = "rails" ]; then
-  bundle
-  yarn
+
+if [ "$1" = "rails" ] || [ "$1" = "rspec" ]; then
+
+  if [ -x bin/setup ]; then
+    bin/setup
+  else
+    bundle
+  fi
 
   if [ "$2" = "server" ] || [ "$2" = "s" ]; then
     rm -f /tmp/pids/server.pid
   fi
+
 fi
 
 exec $@
